ad8a24e7a1ab6378cd07d0990dd0358c052b29c2
Adam West <Adamwest023@gmail.com>
2023-01-13 14:30:12 -0600

    added links for web dev

--- src/pages/Resume/index.js
+++ src/pages/Resume/index.js
@@ -15,19 +15,26 @@ const sevyDoc = () => {
   window.location.href = "https://tv.apple.com/us/episode/hi-im-sevy/umc.cmc.7423yg7w4ajih44dpaa44jxyi?showId=umc.cmc.4wurqdmcfa40pcdr7730pynwy"
 }
 
+const githubAcct = () => {
+  window.location.href = "https://github.com/Adamwest023"
+}
+const runnerGame = () => {
+  window.location.href = "https://adamwest023.github.io/Endless-runner-game/"
+}
+
 const Resume = () => {
   return (
     <>
       <Navbar />
-      <Container >
+      <Container className='resume' >
         <Row className="resume">
           <Col sm={12} md={6} >
             <div className='resume-title' >
               <h2>Film Credits</h2>
-              <h6 >Director of Photography <br/> <h6 className="film-list">Camera & Drone Operator</h6></h6>
+              <h6 >Director of Photography <br /> <h6 className="film-list">Camera & Drone Operator</h6></h6>
               <p>Tonkawa Documentary</p>
-              <p><a href='#'onClick={travisDoc}>Hi I'm Travis</a></p>
-              <p><a href='#'onClick={sevyDoc}>Hi I'm Sevy</a></p>
+              <p><a href='#' onClick={travisDoc}>Hi I'm Travis</a></p>
+              <p><a href='#' onClick={sevyDoc}>Hi I'm Sevy</a></p>
               <p>Water Wars</p>
               <p><a href="https://thewellaustin.com/stories/">Stories at the Well</a></p>
               <p>Cycle Breakers season 1</p>
@@ -46,6 +53,14 @@ const Resume = () => {
           <Col>
             <div className='resume-title'>
               <h2>Web Projects</h2>
+              <p></p>
+              <p className="film-list"><a href="#" onClick={githubAcct}>Github Account</a></p>
+              <p>Adamwestcreative.com</p>
+              <p>React Fest React project</p>
+              <p>Git-a-recipe Api Project</p>
+              <p>Weather Api Project</p>
+              <p><a href="#" onClick={runnerGame}>Python Runner Game </a></p>
+              <p>Pyshooter Python game</p>
             </div>
           </Col>
         </Row>
