caaf496f3db0714fc10d4c01b3d924066dd770a7
Adam West <Adamwest023@gmail.com>
2023-07-12 16:19:23 -0500

    moved access point of reels to s3

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -5,13 +5,19 @@ import "bootstrap/dist/css/bootstrap.min.css"
 import WebReel from '../../assets/videos/WebReel.mp4'
 import DroneReel from '../../assets/videos/DroneReel.mp4'
 
+const webReel = () => {
+    window.location.href = "https://myvideobucket2323.s3.us-east-2.amazonaws.com/WebReel.mp4"
+}
+const droneUrl = () => {
+    window.location.href = "https://myvideobucket2323.s3.us-east-2.amazonaws.com/DroneReel.mp4"}
+
 
 const Header = () => {
     return (
         <div className='home' >
             <div className='overlay'></div>
-            <video className="video1" src={WebReel} type="video/mp4" autoPlay loop muted />
-            <video className="video2" src={DroneReel} type="video/mp4" autoPlay loop muted />
+            <video className="video1" url={webReel} type="video/mp4" autoPlay loop muted />
+            <video className="video2" url={droneUrl} type="video/mp4" autoPlay loop muted />
         </div>
     )
 }
