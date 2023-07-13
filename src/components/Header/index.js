8c17ed65e96b42083c1680d48742e74eb6ce5fea
Adam West <Adamwest023@gmail.com>
2023-07-13 16:41:02 -0500

    updated favicon

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -2,15 +2,18 @@ import React from 'react';
 // import Footer from '../../components/Footer'
 //new components for bootstrap
 import "bootstrap/dist/css/bootstrap.min.css"
-import WebReel from '../../assets/videos/WebReel.mp4'
+// import WebReel from '../../assets/videos/WebReel.mp4'
 import DroneReel from '../../assets/videos/DroneReel.mp4'
 
 
+const webReel = () => {
+    window.location.href= "https://myvideobucket2323.s3.us-east-2.amazonaws.com/WebReel.mp4"
+}
 const Header = () => {
     return (
         <div className='home' >
             <div className='overlay'></div>
-            <video className="video1" src={WebReel} type="video/mp4" autoPlay loop muted />
+            <video className="video1" url={webReel} type="video/mp4" autoPlay loop muted />
             <video className="video2" src={DroneReel} type="video/mp4" autoPlay loop muted />
         </div>
     )
