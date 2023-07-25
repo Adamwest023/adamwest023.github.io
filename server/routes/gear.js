3ea5952e0897ae576765525e5e25e37ba86ddd3c
Adam West <Adamwest023@gmail.com>
2023-07-25 12:55:45 -0500

    completed router for gear

--- server/routes/gear.js
+++ server/routes/gear.js
@@ -6,7 +6,7 @@ const router = express.Router();
 //CREATE
 router.post("/", async (req, res) => {
 
-    const newGear = new Gear(req.body) 
+    const newGear = new Gear(req.body)
     try {
         const savedGear = await newGear.save()
         res.status(200).json(savedGear)
@@ -15,7 +15,47 @@ router.post("/", async (req, res) => {
     }
 });
 //UPDATE
+router.put("/:id", async (req, res) => {
+    try {
+        const updatedGear = await Gear.findByIdAndUpdate(
+            req.params.id,
+            { $set: req.body },
+            { new: true })
+        res.status(200).json(updatedGear)
+    } catch {
+        res.status(500).json(err)
+    }
+});
 //DELETE
+router.delete("/:id", async (req, res) => {
+    try {
+        await Gear.findByIdAndDelete(
+            req.params.id
+        );
+        res.status(200).json("Gear Deleted");
+    } catch {
+        res.status(500).json(err)
+    }
+});
 //GET
+router.get("/:id", async (req, res) => {
+    try {
+        const gear = await Gear.findById(
+            req.params.id
+        );
+        res.status(200).json(gear)
+    } catch {
+        res.status(500).json(err)
+    }
+});
 //GETALL
+router.get("/", async (req, res) => {
+    try {
+        const gear = await Gear.find(
+        );
+        res.status(200).json(gear)
+    } catch {
+        res.status(500).json(err)
+    }
+});
 export default router
