aad93f4d1a032a7cfc08c73201e5d4d0a9350441
Adam West <Adamwest023@gmail.com>
2023-07-25 11:09:50 -0500

    created routes

--- server/index.js
+++ server/index.js
@@ -1,6 +1,9 @@
 import express from "express";
 import dotenv from "dotenv";
 import mongoose from "mongoose";
+import authRoute from "./routes/auth.js"
+import userRoute from "./routes/users.js"
+import gearRoute from "./routes/gear.js"
 
 const app = express();
 dotenv.config();
@@ -18,13 +21,21 @@ const connect = async () => {
 mongoose.connection.on("disconnected",()=>{
   console.log("mongoDB disconnected!")
 });
-mongoose.connection.on("connected",()=>{
-  console.log("mongoDB  connected!")
+
+
+
+app.get('/users', (req,res) => {
+  res.send("hello first request!")
 })
 
+//middlewares
+app.use("/api/auth", authRoute);
+app.use("/api/user", userRoute);
+app.use("/api/gear", gearRoute);
 
 
-const PORT = process.env.PORT || 3001;
+
+const PORT = process.env.PORT || 8800;
 
 app.listen(PORT, () => {
   connect()
